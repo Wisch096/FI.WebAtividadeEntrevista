c3d32610270fac80676c0fdad8076b397dac3a37
Matheus Wisch <wisch.pg@outlook.com>
2024-07-03 00:12:51 -0300

    feat: ajustes para criar um beneficiario

--- FI.WebAtividadeEntrevista/Controllers/BeneficiariosController.cs
+++ FI.WebAtividadeEntrevista/Controllers/BeneficiariosController.cs
@@ -1,43 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using FI.AtividadeEntrevista.BLL;
-using FI.AtividadeEntrevista.DML;
-using WebAtividadeEntrevista.Models;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using FI.AtividadeEntrevista.BLL;
+    using FI.AtividadeEntrevista.DML;
+    using WebAtividadeEntrevista.Models;
 
-namespace FI.WebAtividadeEntrevista.Controllers
-{
-    public class BeneficiariosController : Controller
+    namespace FI.WebAtividadeEntrevista.Controllers
     {
-        [HttpPost]
-        public JsonResult Incluir(BeneficiariosModel model)
+        public class BeneficiariosController : Controller
         {
-            BoBeneficiario bo = new BoBeneficiario();
+            [HttpPost]
+            public JsonResult Incluir(BeneficiariosModel model)
+            {
+                BoBeneficiario bo = new BoBeneficiario();
+                
+                if (!this.ModelState.IsValid)
+                {
+                    List<string> erros = (from item in ModelState.Values
+                                          from error in item.Errors
+                                          select error.ErrorMessage).ToList();
+
+                    Response.StatusCode = 400;
+                    return Json(string.Join(Environment.NewLine, erros));
+                }
+                else
+                {
+                    model.Id = bo.Incluir(new Beneficiario()
+                    {                    
+                        Nome = model.Nome,
+                        CPF = model.CPF
+                       
+                    });
+                    
+                    return Json("Cadastro efetuado com sucesso");
+                }
+            }
             
-            if (!this.ModelState.IsValid)
-            {
-                List<string> erros = (from item in ModelState.Values
-                                      from error in item.Errors
-                                      select error.ErrorMessage).ToList();
-
-                Response.StatusCode = 400;
-                return Json(string.Join(Environment.NewLine, erros));
-            }
-            else
-            {
-                model.Id = bo.Incluir(new Beneficiario()
-                {                    
-                    Nome = model.Nome,
-                    CPF = model.CPF,
-                    IdCliente = model.IdCliente,
-                   
-                });
-
-           
-                return Json("Cadastro efetuado com sucesso");
-            }
         }
-        
     }
-}
