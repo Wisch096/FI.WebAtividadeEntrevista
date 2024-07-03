837c3928a93cf3c2a86bf001f07111c5165dc251
Matheus Wisch <wisch.pg@outlook.com>
2024-07-02 23:11:14 -0300

    feat: criaçao da model de beneficiarios

---
+++ FI.WebAtividadeEntrevista/Models/BeneficiariosModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FI.WebAtividadeEntrevista.Validations;
+
+namespace WebAtividadeEntrevista.Models
+{
+    public class BeneficiariosModel
+    {
+        public long Id { get; set; }
+        
+        /// <summary>
+        /// Nome
+        /// </summary>
+        [Required]
+        public string Nome { get; set; }
+        
+        /// <summary>
+        /// CPF
+        /// </summary>
+        [Required]
+        [CPFValidation]
+        public string CPF { get; set; }
+
+        /// <summary>
+        /// Id do cliente
+        /// </summary>
+        [Required]
+        public string IDCliente { get; set; }
+        
+    }
+}
