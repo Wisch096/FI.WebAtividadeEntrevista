5ba72726607ca68f8f9c3713945eb14ff1d7570e
Matheus Wisch <wisch.pg@outlook.com>
2024-07-02 23:33:52 -0300

    feat: criaçao da estrutura necessaria para salvar o beneficiario

---
+++ FI.AtividadeEntrevista/BLL/BoBeneficiario.cs
@@ -0,0 +1,15 @@
+﻿namespace FI.AtividadeEntrevista.BLL
+{
+    public class BoBeneficiario
+    {
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public long Incluir(DML.Beneficiario beneficiario)
+        {
+            DAL.DaoBeneficiario cli = new DAL.DaoBeneficiario();
+            return cli.Incluir(beneficiario);
+        }
+    }
+}
---
+++ FI.AtividadeEntrevista/DAL/Beneficiarios/DaoBeneficiario.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using FI.AtividadeEntrevista.DML;
+
+namespace FI.AtividadeEntrevista.DAL
+{
+    internal class DaoBeneficiario : AcessoDados
+    {
+        /// <summary>
+        /// Inclui um novo beneficiário
+        /// </summary>
+        /// <param name="beneficiario">Objeto de beneficiário</param>
+        internal long Incluir(DML.Beneficiario beneficiario)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Nome", beneficiario.Nome));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Sobrenome", beneficiario.CPF));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CPF", beneficiario.IdCliente));
+            
+            DataSet ds = base.Consultar("FI_SP_IncBenef", parametros);
+            long ret = 0;
+            if (ds.Tables[0].Rows.Count > 0)
+                long.TryParse(ds.Tables[0].Rows[0][0].ToString(), out ret);
+            return ret;
+        }
+        
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        internal void Alterar(DML.Cliente cliente)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Nome", cliente.Nome));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Sobrenome", cliente.Sobrenome));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CPF", cliente.CPF));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Nacionalidade", cliente.Nacionalidade));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CEP", cliente.CEP));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Estado", cliente.Estado));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Cidade", cliente.Cidade));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Logradouro", cliente.Logradouro));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Email", cliente.Email));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Telefone", cliente.Telefone));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("ID", cliente.Id));
+
+            base.Executar("FI_SP_AltCliente", parametros);
+        }
+
+
+        /// <summary>
+        /// Excluir Cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        internal void Excluir(long Id)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Id", Id));
+
+            base.Executar("FI_SP_DelCliente", parametros);
+        }
+        
+    }
+}
